5afa8ff87d05be83f7d14070f013827ffa1dd6ef
Isabella Mighell <ikm4dm@virginia.edu>
2021-08-09 18:51:58 -0400

    commit tree 85128e37ebdb4a514d10b248eaf9763d8ba6bfda

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let multiply;
+let num1 = 2;
+let num2 = 31;
+multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*100) + 1;
+
+let num3 = 24;
+let num4= 5;
+let mod = num3 % num4;
+
+let max = Math.max(7, 20, 8, 17, 11, 13);
